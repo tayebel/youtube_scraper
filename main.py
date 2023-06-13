cbf866dc4ed38f3fdca85d986e222711d5bc1d04
tayebel <t.elmehadji@gmail.com>
2023-06-13 16:21:34 +0200

    3

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from googleapiclient.discovery import build
 api_key='AIzaSyCOjfTLdnQOUjGJD_OCNhX0O_6UyIAPG5U'
-
+from googleapiclient.discovery import build
+import random as rd
+import string
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import dash
+import matplotlib.pyplot as plt
+from dash import Dash, html, dcc,dash_table,callback,Output,Input
+import plotly.graph_objs as go
+import plotly.express as px
+import openpyxl
 import random as rd
 import string
 import numpy as np
@@ -52,7 +63,7 @@ class yt_analysis:
     def yt_search(self):
         
         if self.maxResults==None:
-            self.maxResults=25 
+            self.maxResults=25
             
         if self.keyword==None and self.channelId==None:
             self.keyword=self.get_keyword()  
@@ -133,7 +144,8 @@ class yt_analysis:
         plt.subplot(1,3,3)
         bar_views=sns.barplot(x='channel_name',y='views',data=df)
         plt.xticks(rotation=45)
-    
+        return  df
+       
     def excel(self):
         import openpyxl
         self.data ,self.username ,self.video_total, self.subscriber, self.view =self.chan_stats()
@@ -155,10 +167,110 @@ class yt_analysis:
             
         self.sheet.sheet_format.defaultColWidth  = 20
         return self.workbook.save("youtube.xlsx")
+
+
+class dashboard(yt_analysis):
+    def __init__(self, channelType=None, eventType=None, location=None, locationRadius=None,
+                 maxResults=None, channelId=None, regions_code=None, keyword=None, order=None,
+                 publishedAfter=None, publishedBefore=None, relevanceLanguage=None):
+        super().__init__(channelType, eventType, location, locationRadius, maxResults, channelId,
+                         regions_code, keyword, order, publishedAfter, publishedBefore, relevanceLanguage)
+
+    def board(self):
+        self.data ,self.username ,self.video_total, self.subscriber, self.view =self.chan_stats()
+        app = Dash(__name__)
+        import plotly.express as px
+
+        from dash.exceptions import PreventUpdate
+
+        app.layout = html.Div(children=[
+            html.H1(children='Youtube analysis dashboard: '),
+            html.Div(children='''This dash is A web application framework to visualize and analyze different youtube channels either random ones or chosing a specific ones through entering  the inputs (e.g channel username)) '''),
+
+            html.Label('Select Inputs to Enter:'),
+
            
-yt=yt_analysis(keyword='gg',regions_code='DZ')
-yt.excel()           
-  
+
+            html.Label('Enter the selected inputs:'),
+
+            html.Div(id='input-fields'),
+
+            html.Button('Save Excel Sheet', id='save-button'),
+            
+            html.Div(id='body-div'),
+
+            
+            dcc.RadioItems(options=['number_of_videos', 'views', 'subscribers'], value='views', id='controls-and-radio-item'),
+            dash_table.DataTable(data=self.data, page_size=6),
+            dcc.Graph(figure={}, id='controls-and-graph'),
+            dcc.Graph(figure={}, id='controls')
+            
+            
+
+
+            
+        ])
+        
+        
+ 
+        @callback(
+           
+            Output(component_id='controls-and-graph', component_property='figure'),
+            
+            Input(component_id='controls-and-radio-item', component_property='value'))
+        
+        
+        def update_graph(col_chosen):
+            fig = px.histogram(self.data, x='channel_name', y=col_chosen, histfunc='avg')
+          
+            return fig
+        
+        
+        @callback(
+           
+            Output(component_id='controls', component_property='figure'),
+            
+            Input(component_id='controls-and-radio-item', component_property='value'))
+        
+        def update_graph(col_chosen):
+            
+            fig2= px.pie(self.data, values=col_chosen, names='channel_name')
+            return fig2
+        @callback(
+            Output(component_id='body-div', component_property='children'),
+           
+            Input(component_id='save-button', component_property='n_clicks'))
+            
+        
+        def update_output(n_clicks):
+            if n_clicks is None:
+                raise PreventUpdate
+            else:
+                return self.excel()
+        
+       
+            
+        app.run_server(port=8053)
+
+ds = dashboard(maxResults=25,keyword='cheb khaled')
+ds.board()
+
+
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
 
              
          
